d930ea279e4c3faab918d7446c9519905f3cae61
Francisco Lizcano <flizcanor@icloud.com>
2025-10-29 09:18:56 -0500

    Update Sintesis.vue

--- src/views/curso/Sintesis.vue
+++ src/views/curso/Sintesis.vue
@@ -2,7 +2,7 @@
   .curso-main-container.creditos-vista
     BannerInterno(subTitulo="SÍNTESIS")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-      p(data-aos="fade-up").mb-5 La unidad 2: Valores límites permisibles, aborda los principales tipos de agentes químicos presentes en los entornos laborales, sus características y los efectos que pueden generar sobre la salud. Esta unidad ofrece una visión detallada de las diferentes formas en que estos contaminantes ingresan al organismo, así como de los mecanismos de absorción y los factores que influyen en su toxicidad. Este enfoque integral permite a los estudiantes comprender los riesgos asociados a la exposición química en el trabajo y aplicar medidas preventivas efectivas para proteger la salud ocupacional.
+      p(data-aos="fade-up").mb-5 La unidad 2: Mecanismos de transmisión de riesgos biológicos, tiene como objetivo comprender los mecanismos de transmisión de los riesgos biológicos con el fin de identificar las vías por las cuales los agentes biológicos pueden propagarse y afectar la salud humana. Este entendimiento es esencial para implementar medidas efectivas de prevención y control tanto en los entornos laborales como en los comunitarios.
 
       .row.justify-content-center
         .col-lg-12.mb-5
